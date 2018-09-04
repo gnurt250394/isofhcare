05bc7b218172357b8baea2e78036beea53df48a6
mainam <mainam.ctk33@gmail.com>
2018-09-04 11:26:09 +0700

    modify realm models, upgrate lib date-utils

--- src/models/realm-models.js
+++ src/models/realm-models.js
@@ -1,69 +1,44 @@
+const SearchHistory =
+{
+    name: 'SearchHistory',
+    primaryKey: 'id',
+    properties: {
+        id: 'string',
+        dataId: 'string',
+        type: 'int',
+        name: 'string',
+        timeSearch: 'double',
+        data: 'string?',
+        userId: 'string?'
+    }
+}
+const DataString =
+{
+    name: "DataString",
+    primaryKey: "key",
+    properties: {
+        key: 'string',
+        value: 'string'
+    }
+}
+const Specialist =
+{
+    name: "Specialist",
+    primaryKey: "id",
+    properties: {
+        id: 'string',
+        name: 'string',
+        nameSearch: 'string',
+        data: 'string'
+    }
+}
 module.exports = {
     DRUG_HISTORY: 1,
     FACILITY_HISTORY: 2,
     LOCATION_HISTORY: 3,
     schemaVersion: 4,
-    SearchHistory:
-    {
-        name: 'SearchHistory',
-        primaryKey: 'id',
-        properties: {
-            id: 'string',
-            dataId: 'string',
-            type: 'int',
-            name: 'string',
-            timeSearch: 'double',
-            data: 'string?',
-            userId: 'string?'
-        }
-    },
-    DataString:
-    {
-        name: "DataString",
-        primaryKey: "key",
-        properties: {
-            key: 'string',
-            value: 'string'
-        }
-    },
-    Specialist:
-    {
-        name: "Specialist",
-        primaryKey: "id",
-        properties: {
-            id: 'string',
-            name: 'string',
-            nameSearch: 'string',
-            data: 'string'
-        }
-    },
-    Schemas: [{
-        name: 'SearchHistory',
-        primaryKey: 'id',
-        properties: {
-            id: 'string',
-            dataId: 'string',
-            type: 'int',
-            name: 'string',
-            timeSearch: 'double',
-            data: 'string?',
-            userId: 'string?'
-        }
-    }, {
-        name: "DataString",
-        primaryKey: "key",
-        properties: {
-            key: 'string',
-            value: 'string'
-        }
-    }, {
-        name: "Specialist",
-        primaryKey: "id",
-        properties: {
-            id: 'string',
-            name: 'string',
-            nameSearch: 'string',
-            data: 'string'
-        }
-    }]
+    SearchHistory,
+    DataString,
+    Specialist,
+    Schemas: [SearchHistory, DataString, Specialist]
 }
