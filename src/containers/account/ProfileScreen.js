ec44fba231bbae5f0c993f6aa5c12dbae995a801
tuantq <tuan.tq@isofh.com>
2018-09-26 17:44:33 +0700

    finish Animated sceen profile

--- src/containers/account/ProfileScreen.js
+++ src/containers/account/ProfileScreen.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import ActivityPanel from '@components/ActivityPanel';
-import { View, Text, ScrollView, TouchableOpacity, StyleSheet, Animated, Easing, Platform } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, StyleSheet, TextInput, Animated, Easing, Platform, ImageBackground } from 'react-native';
 import { Fab, Container, Header } from 'native-base';
 import { connect } from 'react-redux';
 import ScaleImage from 'mainam-react-native-scaleimage';
@@ -14,7 +14,7 @@ import userProvider from '@data-access/user-provider';
 import redux from '@redux-store';
 import UserInput from '@components/UserInput';
 import constants from '@resources/strings';
-
+import ic_back from '@images/ic_back.png';
 const { height: DEVICE_HEIGHT, width: DEVICE_WIDTH } = Dimensions.get('window');
 
 const bgImage = require("@images/bg.png")
@@ -29,8 +29,11 @@ class ProfileScreen extends Component {
             view: true,
             user: this.props.userApp.currentUser
         };
-        // alert(JSON.stringify(Platform.OS))
+        this.onChange = this.onChange.bind(this)
+        this.animatedValueBtn = new Animated.Value(54)
+        this.selectAvatar = this.selectAvatar.bind(this)
     }
+
     componentWillMount() {
         this.animatedValue = new Animated.Value(0);
         this.value = 0;
@@ -69,10 +72,33 @@ class ProfileScreen extends Component {
                 tension: 10
             }).start();
         }
+    }
 
+    animate() {
+        let value = 0
+        this.state.view ? value = 300 : value = 54
+        Animated.timing(
+            this.animatedValueBtn, {
+                toValue: value,
+                duration: 250,
+                easing: Easing.linear
+            }
+        ).start(() => { console.log("Animated ") })
+    }
+
+    onChange() {
+        this.animate()
+        this.flipCard()
+        this.setState({ view: !this.state.view })
+    }
+
+    selectAvatar() {
+        alert("GO Her")
     }
 
     render() {
+        const animatedSizeFrom = { width: this.animatedValueBtn }
+        const animatedSizeTo = { width: this.animatedValueBtn }
         const frontAnimatedStyle = {
             transform: [
                 { rotateY: this.frontInterpolate }
@@ -85,58 +111,214 @@ class ProfileScreen extends Component {
         }
 
         return (
-            <View style={styles.container}>
-                <View>
-                    <Animated.View style={[styles.flipCard, frontAnimatedStyle, Platform.OS == "android" ? { opacity: this.frontOpacity } : null]}>
-                        {
-                            Platform.OS == "android" ?
-                                <Text style={styles.flipText}> Android Display </Text>
-                                :
-                                <Text style={styles.flipText}> IOS Display </Text>
-                        }
+            <ActivityPanel style={{ flex: 1 }} title="Thông tin cá nhân" showFullScreen={true}>
+                <ImageBackground source={bgImage} style={{ width: '100%', height: '100%' }}>
 
-                    </Animated.View>
-                    <Animated.View style={[styles.flipCard, styles.flipCardBack, backAnimatedStyle, Platform.OS == "android" ? { opacity: this.backOpacity } : null]}>
-                        {
-                            Platform.OS == "android" ?
-                                <Text style={styles.flipText}> Android Edit </Text>
-                                :
-                                <Text style={styles.flipText}> IOS Edit </Text>
-                        }
-                    </Animated.View>
-                </View>
-                <TouchableOpacity onPress={() => this.flipCard()}>
-                    <Text>Flip!</Text>
-                </TouchableOpacity>
-            </View>
+                    <ScrollView style={styles.container} zIndex={2}>
+                        <View style={styles.header}>
+                            <TouchableOpacity style={styles.boxAvatar} onPress={this.selectAvatar}>
+                                <ScaleImage source={icSupport} width={80} style={styles.avatar} />
+                                <ScaleImage source={icCamera} width={20} style={styles.iconChangeAvatar} />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.boxAnimatedcontent}>
+                            <Animated.View style={[styles.flipCard, frontAnimatedStyle, Platform.OS == "android" ? { opacity: this.frontOpacity } : null]}>
+                                <View>
+                                    <Text style={styles.name}>{this.state.user.name.toUpperCase()}</Text>
+                                    <View style={styles.content}>
+                                        <View style={styles.item}>
+                                            <Text style={styles.lable}>Số điện thoại:</Text>
+                                            <Text style={styles.value}>{this.state.user.phone ? this.state.user.phone : 'Chưa Có Số Điện Thoại'}</Text>
+                                        </View>
+                                        <View style={styles.item}>
+                                            <Text style={styles.lable}>Email:</Text>
+                                            <Text style={styles.value}>{this.state.user.email ? this.state.user.email : 'Chưa Có Email'}</Text>
+                                        </View>
+                                    </View>
+                                </View>
+                            </Animated.View>
+                            <Animated.View style={[styles.flipCard2, styles.flipCardBack, backAnimatedStyle, Platform.OS == "android" ? { opacity: this.backOpacity } : null]}>
+                                <TextInput
+                                    placeholder={constants.input_username_or_email}
+                                    value={this.state.user.name}
+                                    autoCapitalize={'none'}
+                                    returnKeyType={'next'}
+                                    autoCorrect={false}
+                                    onTextChange={(s) => this.setState({ email: s })}
+                                    style={styles.inputText}
+                                />
+                                <TextInput
+                                    placeholder={constants.input_username_or_email}
+                                    value={this.state.user.email}
+                                    autoCapitalize={'none'}
+                                    returnKeyType={'next'}
+                                    autoCorrect={false}
+                                    onTextChange={(s) => this.setState({ email: s })}
+                                    style={styles.inputText}
+                                />
+                                <TextInput
+                                    placeholder={constants.input_username_or_email}
+                                    value={this.state.user.name}
+                                    autoCapitalize={'none'}
+                                    returnKeyType={'next'}
+                                    autoCorrect={false}
+                                    onTextChange={(s) => this.setState({ email: s })}
+                                    style={styles.inputText}
+                                />
+
+                                <Text style={{ color: "#000000", marginTop: 20 }}>
+                                    <Text style={{ fontWeight: "bold", color: "#00977c" }}>Lưu ý</Text> : khi thay đổi email, quý khách cần đăng nhập email mới để kích hoạt lại tài khoản
+                                </Text>
+
+                            </Animated.View>
+                            <View style={styles.actions}>
+                                <Animated.View style={[styles.boxAnimate, this.state.view ? animatedSizeTo : animatedSizeFrom]}>
+                                    <TouchableOpacity style={styles.fab} onPress={this.onChange} >
+                                        {
+                                            this.state.view ?
+                                                <ScaleImage source={icEdit} width={24} style={{ position: 'absolute', left: 15, top: 15 }} />
+                                                :
+                                                <Text style={styles.btnUpdate}>CẬP NHẬT</Text>
+                                        }
+                                    </TouchableOpacity>
+                                </Animated.View>
+                            </View>
+                        </View>
+                    </ScrollView>
+                </ImageBackground>
+            </ActivityPanel>
         );
     }
 }
 
 const styles = StyleSheet.create({
+
     container: {
-        flex: 1,
+        flex: 1
+    },
+    header: {
+        // backgroundColor: "white",
+        paddingTop: 40,
+        paddingLeft: 60,
+        height: "100%",
+        flex: 1
+    },
+    boxAvatar: {
+        width: 84,
+        height: 84,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 100,
+        borderWidth: 2,
+        borderColor: '#5fba7d',
+        marginBottom: 18,
+    },
+    avatar: {
+        alignSelf: 'center',
+        borderRadius: 50
+    },
+    iconChangeAvatar: {
+        position: 'absolute',
+        zIndex: 1,
+        bottom: 2,
+        right: 2
+    },
+    boxAnimatedcontent: {
         alignItems: "center",
         justifyContent: "center",
     },
     flipCard: {
-        width: 200,
-        height: 200,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'blue',
+        width: 320,
+        height: 350,
+        // backgroundColor: 'blue',
+        backgroundColor: '#edf4fa',
+        borderRadius: 20,
         backfaceVisibility: 'hidden',
     },
+    flipCard2: {
+        width: 320,
+        height: 350,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backfaceVisibility: 'hidden',
+        padding: 20
+    },
     flipCardBack: {
-        backgroundColor: "red",
+        // backgroundColor: "red",
+        backgroundColor: '#edf4fa',
+        borderRadius: 20,
         position: "absolute",
         top: 0,
+        padding: 20
     },
-    flipText: {
-        width: 90,
-        fontSize: 20,
-        color: 'white',
-        fontWeight: 'bold',
+    actions: {
+        height: 74,
+        width: 300,
+        alignItems: 'flex-end',
+    },
+    name: {
+        marginTop: 20,
+        paddingLeft: 20,
+        // marginBottom: 40,
+        fontSize: 26,
+        fontWeight: "600",
+        fontStyle: "normal",
+        letterSpacing: 0,
+        color: "#23429b",
+        width: 190,
+    },
+    content: {
+        padding: 20,
+        borderRadius: 20,
+        backgroundColor: '#edf4fa',
+        paddingBottom: 40
+    },
+    item: {
+        paddingTop: 20,
+        // color:'#3a4f60'
+    },
+    lable: {
+        fontSize: 19,
+        color: '#3a4f60'
+    },
+    value: {
+        fontSize: 19,
+        fontWeight: "bold",
+        color: '#3a4f60'
+    },
+    boxAnimate: {
+        marginTop: 20,
+        width: 54,
+    },
+    fab: {
+        width: "100%",
+        height: 54,
+        backgroundColor: '#00796b',
+        borderRadius: 100
+    },
+    btnUpdate: {
+        width: '100%',
+        height: '100%',
+        textAlign: 'center',
+        paddingTop: 15,
+        fontSize: 18,
+        fontWeight: "bold",
+        fontStyle: "normal",
+        color: '#ffffff',
+        letterSpacing: 0
+    },
+    inputText: {
+        marginTop: 20,
+        backgroundColor: 'white',
+        width: '100%',
+        borderRadius: 6,
+        fontWeight: "bold",
+        fontStyle: "normal",
+        letterSpacing: 0,
+        color: "#3a4f60",
+        height: 42,
+        borderWidth: 1,
+        borderColor: 'rgba(155,155,155,0.7)'
     }
 });
 
