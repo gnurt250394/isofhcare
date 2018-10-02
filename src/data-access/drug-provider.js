236b4a05266dfa4d785032d04a8d6edb353da657
mainam <mainam.ctk33@gmail.com>
2018-10-02 11:23:55 +0700

    sort type when get top drug

--- src/data-access/drug-provider.js
+++ src/data-access/drug-provider.js
@@ -15,13 +15,19 @@ module.exports = {
                     this.getTop(top, callback, true);
             });
         else
-            this.search("", 1, top, (s, e) => {
+            this.searchTop("", 1, top, (s, e) => {
                 if (s && s.code == 0 && s.data && s.data.data) {
                     datacacheProvider.save("", constants.key.storage.DATA_TOP_DRUG, s.data.data);
                     if (callback)
                         callback(s.data.data, e);
                 }
             });
+    },
+    searchTop(name, page, size, callback) {
+        client.requestApi("get", constants.api.drug.search + "?name=" + name + "&page=" + page + "&size=" + size + "&sortType=1", {}, (s, e) => {
+            if (callback)
+                callback(s, e);
+        });
     },
 
     search(name, page, size, callback) {
