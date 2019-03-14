0e58a2d39dcf9799cdf0fcd949b786d784920c8f
mainam <mainam.ctk33@gmail.com>
2019-03-15 01:38:31 +0700

    show button notification in home page'

--- src/components/DrawerContent.js
+++ src/components/DrawerContent.js
@@ -1,26 +1,34 @@
-import React, {
-  Component
-} from 'react';
-import { View, Image, StyleSheet, Text, TouchableOpacity, Dimensions, ScrollView, Platform, Linking } from 'react-native';
-const { width, height } = Dimensions.get('window');
-import { connect } from 'react-redux';
-import ScaleImage from 'mainam-react-native-scaleimage';
-import ActivityPanel from '@components/ActivityPanel';
-import { NavigationActions } from 'react-navigation'
-import ActionBar from '@components/Actionbar';
-import constants from '@resources/strings';
-import { isIphoneX } from 'react-native-iphone-x-helper';
-import redux from '@redux-store';
-import ImageLoad from 'mainam-react-native-image-loader';
-import snackbar from '@utils/snackbar-utils';
+import React, { Component } from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  ScrollView,
+  Platform,
+  Linking
+} from "react-native";
+const { width, height } = Dimensions.get("window");
+import { connect } from "react-redux";
+import ScaleImage from "mainam-react-native-scaleimage";
+import ActivityPanel from "@components/ActivityPanel";
+import { NavigationActions } from "react-navigation";
+import ActionBar from "@components/Actionbar";
+import constants from "@resources/strings";
+import { isIphoneX } from "react-native-iphone-x-helper";
+import redux from "@redux-store";
+import ImageLoad from "mainam-react-native-image-loader";
+import snackbar from "@utils/snackbar-utils";
 
-
-const resetAction = (routeName) => NavigationActions.reset({
-  index: 0,
-  actions: [
-    NavigationActions.navigate({ routeName: routeName, drawer: 'close' }),
-  ]
-});
+const resetAction = routeName =>
+  NavigationActions.reset({
+    index: 0,
+    actions: [
+      NavigationActions.navigate({ routeName: routeName, drawer: "close" })
+    ]
+  });
 
 class DrawerContent extends Component {
   constructor(props) {
@@ -28,12 +36,11 @@ class DrawerContent extends Component {
   }
 
   onNavigate(route) {
-    this.props.navigation.dispatch(resetAction(route))
+    this.props.navigation.dispatch(resetAction(route));
   }
 
   hideDrawer() {
-    if (this.props.drawer)
-      this.props.drawer.close();
+    if (this.props.drawer) this.props.drawer.close();
   }
 
   logout() {
@@ -44,45 +51,112 @@ class DrawerContent extends Component {
   }
   render() {
     const icSupport = require("@images/ichotro.png");
-    const source = this.props.userApp.currentUser.avatar ? { uri: this.props.userApp.currentUser.avatar.absoluteUrl() } : icSupport;
+    const source = this.props.userApp.currentUser.avatar
+      ? { uri: this.props.userApp.currentUser.avatar.absoluteUrl() }
+      : icSupport;
     return (
       <View style={styles.container}>
-        <ActionBar icBack={require("@images/icclose.png")} backButtonClick={() => { this.hideDrawer() }} styleBackButton={{ marginLeft: 20 }} actionbarTextColor={[{ color: constants.colors.actionbar_title_color }]} actionbarStyle={[{ paddingTop: isIphoneX() ? 40 : 32, backgroundColor: constants.colors.actionbar_color }, this.props.actionbarStyle, { elevation: 1 }]} />
+        <ActionBar
+          icBack={require("@images/icclose.png")}
+          backButtonClick={() => {
+            this.hideDrawer();
+          }}
+          styleBackButton={{ marginLeft: 20 }}
+          actionbarTextColor={[
+            { color: constants.colors.actionbar_title_color }
+          ]}
+          actionbarStyle={[
+            {
+              paddingTop: isIphoneX() ? 40 : 32,
+              backgroundColor: constants.colors.actionbar_color
+            },
+            this.props.actionbarStyle,
+            { elevation: 1 }
+          ]}
+        />
         <View style={{ width: 250, paddingTop: 10, flex: 1 }}>
-          {this.props.userApp.isLogin ?
+          {this.props.userApp.isLogin ? (
             <View>
-              <TouchableOpacity style={styles.menu_item} onPress={() => { this.props.navigation.navigate("profile") }}>
+              <TouchableOpacity
+                style={styles.menu_item}
+                onPress={() => {
+                  this.props.navigation.navigate("profile");
+                }}
+              >
                 <ImageLoad
                   resizeMode="cover"
                   imageStyle={{ borderRadius: 35 }}
                   borderRadius={35}
-                  customImagePlaceholderDefaultStyle={{ width: 70, height: 70, alignSelf: 'center' }}
+                  customImagePlaceholderDefaultStyle={{
+                    width: 70,
+                    height: 70,
+                    alignSelf: "center"
+                  }}
                   placeholderSource={icSupport}
-                  style={{ width: 70, height: 70, alignSelf: 'center' }}
+                  style={{ width: 70, height: 70, alignSelf: "center" }}
                   resizeMode="cover"
-                  loadingStyle={{ size: 'small', color: 'gray' }}
+                  loadingStyle={{ size: "small", color: "gray" }}
                   source={source}
                   defaultImage={() => {
-                    return <ScaleImage resizeMode='cover' source={icSupport} width={70} style={{ width: 70, height: 70, alignSelf: 'center' }} />
+                    return (
+                      <ScaleImage
+                        resizeMode="cover"
+                        source={icSupport}
+                        width={70}
+                        style={{ width: 70, height: 70, alignSelf: "center" }}
+                      />
+                    );
                   }}
                 />
-                <Text style={{ alignSelf: 'center', fontWeight: 'bold', fontSize: 15, color: 'rgb(35,66,155)', marginTop: 18, }}>{this.props.userApp.currentUser.name ? this.props.userApp.currentUser.name.toUpperCase() : ""}</Text>
+                <Text
+                  style={{
+                    alignSelf: "center",
+                    fontWeight: "bold",
+                    fontSize: 15,
+                    color: "rgb(35,66,155)",
+                    marginTop: 18
+                  }}
+                >
+                  {this.props.userApp.currentUser.name
+                    ? this.props.userApp.currentUser.name.toUpperCase()
+                    : ""}
+                </Text>
               </TouchableOpacity>
-            </View> : null
-          }
+            </View>
+          ) : null}
           <ScrollView style={{ flex: 1, marginLeft: 60, marginTop: 30 }}>
-            {this.props.userApp.isLogin ?
+            {this.props.userApp.isLogin ? (
               <View style={{ marginLeft: 30 }}>
-                <TouchableOpacity style={styles.menu_item} onPress={() => { snackbar.show("Chức năng đang phát triển") }}>
+                <TouchableOpacity
+                  style={styles.menu_item}
+                  onPress={() => {
+                    this.props.navigation.navigate("ehealth");
+                  }}
+                >
                   <Text style={styles.menu_item_text}>Y bạ điện tử</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.menu_item} onPress={() => { snackbar.show("Chức năng đang phát triển") }}>
+                <TouchableOpacity
+                  style={styles.menu_item}
+                  onPress={() => {
+                    this.props.navigation.navigate("ehealth");
+                  }}
+                >
                   <Text style={styles.menu_item_text}>Lịch khám</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.menu_item} onPress={() => { this.props.navigation.navigate("changePass") }}>
+                <TouchableOpacity
+                  style={styles.menu_item}
+                  onPress={() => {
+                    this.props.navigation.navigate("changePass");
+                  }}
+                >
                   <Text style={styles.menu_item_text}>Đổi mật khẩu</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.menu_item} onPress={() => { snackbar.show("Chức năng đang phát triển") }}>
+                <TouchableOpacity
+                  style={styles.menu_item}
+                  onPress={() => {
+                    snackbar.show("Chức năng đang phát triển");
+                  }}
+                >
                   <Text style={styles.menu_item_text}>Đổi mã bảo mật</Text>
                 </TouchableOpacity>
                 {/* <TouchableOpacity style={styles.menu_item} onPress={() => { snackbar.show("Chức năng đang phát triển")  }}>
@@ -101,9 +175,14 @@ class DrawerContent extends Component {
                 {/* <TouchableOpacity style={styles.menu_item} onPress={() => { this.props.navigation.navigate("myQuestion") }}>
                   <Text style={styles.menu_item_text}>Câu hỏi của tôi</Text>
                 </TouchableOpacity> */}
-                <TouchableOpacity style={styles.menu_item} onPress={() => { this.props.navigation.navigate("notification") }}>
+                {/* <TouchableOpacity
+                  style={styles.menu_item}
+                  onPress={() => {
+                    this.props.navigation.navigate("notification");
+                  }}
+                >
                   <Text style={styles.menu_item_text}>Thông báo</Text>
-                </TouchableOpacity>
+                </TouchableOpacity> */}
                 {/* 
                 <TouchableOpacity style={styles.menu_item}>
                   <Text style={styles.menu_item_text}>Ví iSofH care</Text>
@@ -117,54 +196,108 @@ class DrawerContent extends Component {
                 <TouchableOpacity style={styles.menu_item}>
                   <Text style={styles.menu_item_text}>Tin nhắn</Text>
                 </TouchableOpacity> */}
-              </View> : <TouchableOpacity onPress={() => this.login()} style={{ paddingTop: 10, paddingBottom: 10 }}><Text style={{ color: 'rgb(0,151,124)', fontWeight: 'bold', fontSize: 16 }}>ĐĂNG NHẬP</Text></TouchableOpacity>
-            }
+              </View>
+            ) : (
+              <TouchableOpacity
+                onPress={() => this.login()}
+                style={{ paddingTop: 10, paddingBottom: 10 }}
+              >
+                <Text
+                  style={{
+                    color: "rgb(0,151,124)",
+                    fontWeight: "bold",
+                    fontSize: 16
+                  }}
+                >
+                  ĐĂNG NHẬP
+                </Text>
+              </TouchableOpacity>
+            )}
             {/* <View style={{ marginLeft: 30 }}>
               <TouchableOpacity style={styles.menu_item} onPress={() => { this.props.navigation.navigate("listQuestion") }}>
                 <Text style={styles.menu_item_text}>Tư vấn online</Text>
               </TouchableOpacity>
             </View> */}
-            <View style={{ marginTop: 20, height: 3, backgroundColor: "rgb(234,234,234)", }} />
+            <View
+              style={{
+                marginTop: 20,
+                height: 3,
+                backgroundColor: "rgb(234,234,234)"
+              }}
+            />
             <View style={{ marginLeft: 30, marginTop: 20 }}>
-              <TouchableOpacity style={styles.menu_item} onPress={() => { this.props.navigation.navigate("about") }}>
+              <TouchableOpacity
+                style={styles.menu_item}
+                onPress={() => {
+                  this.props.navigation.navigate("about");
+                }}
+              >
                 <Text style={styles.menu_item_text}>Về iSofH</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.menu_item} onPress={() => {
-                if (Platform.OS == "ios") {
-                  Linking.openURL('itms-apps://itunes.apple.com/us/app/id1428148423')
-                } else {
-                  Linking.openURL('market://details?id=com.isofh.isofhcare')
-                }
-              }}>
+              <TouchableOpacity
+                style={styles.menu_item}
+                onPress={() => {
+                  if (Platform.OS == "ios") {
+                    Linking.openURL(
+                      "itms-apps://itunes.apple.com/us/app/id1428148423"
+                    );
+                  } else {
+                    Linking.openURL("market://details?id=com.isofh.isofhcare");
+                  }
+                }}
+              >
                 <Text style={styles.menu_item_text}>Đánh giá</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.menu_item} onPress={() => {
-                Linking.openURL('mailto:support@isofhcare.vn?subject=Hỗ trợ sử dụng app ISofhCare&body=')
-              }}>
+              <TouchableOpacity
+                style={styles.menu_item}
+                onPress={() => {
+                  Linking.openURL(
+                    "mailto:support@isofhcare.vn?subject=Hỗ trợ sử dụng app ISofhCare&body="
+                  );
+                }}
+              >
                 <Text style={styles.menu_item_text}>Hỗ trợ</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.menu_item} onPress={() => {
-                Linking.openURL('mailto:support@isofhcare.vn?subject=Báo lỗi quá trình sử dụng app ISofhCare&body=')
-              }}>
+              <TouchableOpacity
+                style={styles.menu_item}
+                onPress={() => {
+                  Linking.openURL(
+                    "mailto:support@isofhcare.vn?subject=Báo lỗi quá trình sử dụng app ISofhCare&body="
+                  );
+                }}
+              >
                 <Text style={styles.menu_item_text}>Góp ý báo lỗi</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.menu_item} onPress={() => { this.props.navigation.navigate("policy") }}>
+              <TouchableOpacity
+                style={styles.menu_item}
+                onPress={() => {
+                  this.props.navigation.navigate("policy");
+                }}
+              >
                 <Text style={styles.menu_item_text}>Chính sách riêng tư</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.menu_item} onPress={() => { this.props.navigation.navigate("terms") }}>
+              <TouchableOpacity
+                style={styles.menu_item}
+                onPress={() => {
+                  this.props.navigation.navigate("terms");
+                }}
+              >
                 <Text style={styles.menu_item_text}>Điều khoản sử dụng</Text>
               </TouchableOpacity>
-              {
-                this.props.userApp.isLogin ?
-                  <TouchableOpacity style={styles.menu_item} onPress={() => { this.logout() }}>
-                    <Text style={styles.menu_item_text}>Đăng xuất</Text>
-                  </TouchableOpacity> : null
-              }
+              {this.props.userApp.isLogin ? (
+                <TouchableOpacity
+                  style={styles.menu_item}
+                  onPress={() => {
+                    this.logout();
+                  }}
+                >
+                  <Text style={styles.menu_item_text}>Đăng xuất</Text>
+                </TouchableOpacity>
+              ) : null}
             </View>
           </ScrollView>
         </View>
-
-      </View >
+      </View>
     );
   }
 }
@@ -178,9 +311,9 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     padding: 5
   },
-  menu_item_text:
-  {
-    fontSize: 16, color: 'rgba(0,0,0,0.7)'
+  menu_item_text: {
+    fontSize: 16,
+    color: "rgba(0,0,0,0.7)"
   }
 });
 function mapStateToProps(state) {
--- src/containers/HomeScreen.js
+++ src/containers/HomeScreen.js
@@ -1,125 +1,185 @@
-import React, { Component, PropTypes } from 'react';
-import { Text, StatusBar, View, Image, StyleSheet, TouchableOpacity, Dimensions, DeviceEventEmitter } from 'react-native';
-import { connect } from 'react-redux';
-import userProvider from '@data-access/user-provider';
-import constants from '@resources/strings';
-import redux from '@redux-store';
-import ScalingDrawer from 'mainam-react-native-scaling-drawer';
-import DrawerContent from '@components/DrawerContent';
-import Home from '@containers/home/tab/Home';
-import TabSearch from '@containers/home/tab/TabSearch';
-import Swiper from 'react-native-swiper';
-const { width, height } = Dimensions.get('window');
-import PushController from '@components/notification/PushController'
-import ActivityPanel from '@components/ActivityPanel';
+import React, { Component, PropTypes } from "react";
+import {
+  Text,
+  StatusBar,
+  View,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  DeviceEventEmitter
+} from "react-native";
+import { connect } from "react-redux";
+import userProvider from "@data-access/user-provider";
+import constants from "@resources/strings";
+import redux from "@redux-store";
+import ScalingDrawer from "mainam-react-native-scaling-drawer";
+import DrawerContent from "@components/DrawerContent";
+import Home from "@containers/home/tab/Home";
+import TabSearch from "@containers/home/tab/TabSearch";
+import Swiper from "react-native-swiper";
+const { width, height } = Dimensions.get("window");
+import PushController from "@components/notification/PushController";
+import ActivityPanel from "@components/ActivityPanel";
 let defaultScalingDrawerConfig = {
-	scalingFactor: 0.7,
-	minimizeFactor: 0.7,
-	swipeOffset: 20
+  scalingFactor: 0.7,
+  minimizeFactor: 0.7,
+  swipeOffset: 20
 };
 class SplashScreen extends Component {
-	constructor(props) {
-		super(props);
-	}
-	componentWillMount() {
-		this.props.dispatch({ type: constants.action.create_navigation_global, value: this.props.navigation });
-	}
-	componentDidMount() {
-		DeviceEventEmitter.removeAllListeners('hardwareBackPress')
-		DeviceEventEmitter.addListener('hardwareBackPress', this.handleHardwareBack.bind(this));
-	}
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    this.props.dispatch({
+      type: constants.action.create_navigation_global,
+      value: this.props.navigation
+    });
+  }
+  componentDidMount() {
+    DeviceEventEmitter.removeAllListeners("hardwareBackPress");
+    DeviceEventEmitter.addListener(
+      "hardwareBackPress",
+      this.handleHardwareBack.bind(this)
+    );
+  }
 
-	componentWillUnmount() {
-		DeviceEventEmitter.removeAllListeners('hardwareBackPress')
-	}
+  componentWillUnmount() {
+    DeviceEventEmitter.removeAllListeners("hardwareBackPress");
+  }
 
-	handleHardwareBack = () => {
-		this.props.navigation.pop();
-		return true;
-	}
+  handleHardwareBack = () => {
+    this.props.navigation.pop();
+    return true;
+  };
+  menuCreate() {
+    return (
+      <View>
+        <TouchableOpacity
+          style={{ padding: 10 }}
+          onPress={() => {
+            if (this.props.userApp.isLogin) {
+              this.props.navigation.navigate("notification");
+            } else {
+              this.props.navigation.navigate("login", {
+                nextScreen: { screen: "notification", param: {} }
+              });
+            }
+          }}
+        >
+          <Text>Thông báo</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  render() {
+    return (
+      <ScalingDrawer
+        ref={ref => (this._drawer = ref)}
+        frontStyle={{ height: height + 10 }}
+        content={
+          <DrawerContent
+            navigation={this.props.navigation}
+            drawer={this._drawer}
+          />
+        }
+        {...defaultScalingDrawerConfig}
+      >
+        <ActivityPanel
+          style={[{ flex: 1 }, this.props.style]}
+          titleStyle={{ marginRight: 60 }}
+          imageStyle={{ marginRight: 50 }}
+          image={require("@images/logo_home.png")}
+          icBack={require("@images/icmenu.png")}
+          backButtonClick={() => {
+            this._drawer.open();
+          }}
+          menuButton={this.menuCreate()}
+          // showMessenger={this.props.userApp.isLogin ? true : false}
+          showMessenger={false}
+          badge={0}
+        >
+          <Swiper
+            ref={ref => (this.swiper = ref)}
+            onIndexChanged={index => {
+              this.setState({ tabIndex: index });
+            }}
+            dot={<View />}
+            activeDot={<View />}
+            paginationStyle={{
+              bottom: 30
+            }}
+            loop={false}
+            style={{ flex: 1 }}
+          >
+            <Home
+              navigation={this.props.navigation}
+              style={{ flex: 1 }}
+              drawer={this._drawer}
+            />
+            <View style={{ flex: 1, backgroundColor: "#000" }} />
+            <View style={{ flex: 1, backgroundColor: "#cac" }} />
+            <View style={{ flex: 1, backgroundColor: "#aba" }} />
 
-	render() {
-		return (
-			<ScalingDrawer
-				ref={ref => this._drawer = ref}
-				frontStyle={{ height: height + 10 }}
-				content={<DrawerContent navigation={this.props.navigation} drawer={this._drawer} />}
-				{...defaultScalingDrawerConfig}
-			>
-				<ActivityPanel
-					style={[{ flex: 1 }, this.props.style]}
-					titleStyle={{ marginRight: 60 }}
-					imageStyle={{ marginRight: 50 }}
-					image={require("@images/logo_home.png")}
-					icBack={require("@images/icmenu.png")}
-					backButtonClick={() => { this._drawer.open() }}
-					// showMessenger={this.props.userApp.isLogin ? true : false}
-					showMessenger={false}
-					badge={0}>
-					<Swiper
-						ref={ref => this.swiper = ref}
-						onIndexChanged={index => {
-							this.setState({ tabIndex: index });
-						}}
-						dot={<View />}
-						activeDot={<View />}
-						paginationStyle={{
-							bottom: 30
-						}}
-						loop={false}
-						style={{ flex: 1 }}
-					>
-						<Home navigation={this.props.navigation} style={{ flex: 1 }} drawer={this._drawer} />
-						<View style={{ flex: 1, backgroundColor: '#000' }} />
-						<View style={{ flex: 1, backgroundColor: '#cac' }} />
-						<View style={{ flex: 1, backgroundColor: '#aba' }} />
-
-						{/* <TabSearch navigation={this.props.navigation} style={{ flex: 1 }} drawer={this._drawer} /> */}
-					</Swiper>
-					<View style={{ height: 50, flexDirection: 'row' }} >
-						<TouchableOpacity style={{ flex: 1 }} onPress={this.swipe.bind(this, 0)}>
-							<Text style={{ textAlign: 'center' }}>Home</Text>
-						</TouchableOpacity>
-						<TouchableOpacity style={{ flex: 1 }} onPress={this.swipe.bind(this, 1)}>
-							<Text style={{ textAlign: 'center' }}>Lịch khám</Text>
-						</TouchableOpacity>
-						<TouchableOpacity style={{ flex: 1 }} onPress={this.swipe.bind(this, 2)}>
-							<Text style={{ textAlign: 'center' }}>Dịch vụ</Text>
-						</TouchableOpacity>
-						<TouchableOpacity style={{ flex: 1 }} onPress={this.swipe.bind(this, 3)}>
-							<Text style={{ textAlign: 'center' }}>Tài khoản</Text>
-						</TouchableOpacity>
-					</View>
-					<PushController />
-				</ActivityPanel>
-			</ScalingDrawer>);
-	}
-	swipe(targetIndex) {
-		const currentIndex = this.swiper.state.index;
-		const offset = targetIndex - currentIndex;
-		this.swiper.scrollBy(offset);
-	}
+            {/* <TabSearch navigation={this.props.navigation} style={{ flex: 1 }} drawer={this._drawer} /> */}
+          </Swiper>
+          <View style={{ height: 50, flexDirection: "row" }}>
+            <TouchableOpacity
+              style={{ flex: 1 }}
+              onPress={this.swipe.bind(this, 0)}
+            >
+              <Text style={{ textAlign: "center" }}>Home</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ flex: 1 }}
+              onPress={this.swipe.bind(this, 1)}
+            >
+              <Text style={{ textAlign: "center" }}>Lịch khám</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ flex: 1 }}
+              onPress={this.swipe.bind(this, 2)}
+            >
+              <Text style={{ textAlign: "center" }}>Dịch vụ</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ flex: 1 }}
+              onPress={this.swipe.bind(this, 3)}
+            >
+              <Text style={{ textAlign: "center" }}>Tài khoản</Text>
+            </TouchableOpacity>
+          </View>
+          <PushController />
+        </ActivityPanel>
+      </ScalingDrawer>
+    );
+  }
+  swipe(targetIndex) {
+    const currentIndex = this.swiper.state.index;
+    const offset = targetIndex - currentIndex;
+    this.swiper.scrollBy(offset);
+  }
 }
 
 const styles = StyleSheet.create({
-	picture: {
-		position: 'absolute',
-		top: 0,
-		bottom: 0,
-		left: 0,
-		right: 0,
-		justifyContent: 'center',
-		alignItems: 'center',
-		flex: 1,
-		width: null,
-		height: null,
-		resizeMode: 'cover',
-	},
+  picture: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: "cover"
+  }
 });
 
 function mapStateToProps(state) {
-	return {
-		userApp: state.userApp
-	};
+  return {
+    userApp: state.userApp
+  };
 }
 export default connect(mapStateToProps)(SplashScreen);
