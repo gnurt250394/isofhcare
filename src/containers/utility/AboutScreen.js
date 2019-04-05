e1ac3ea19913d95534622c64b547c74b871a0f08
mainam <mainam.ctk33@gmail.com>
2019-04-05 18:20:59 +0700

    change about us

--- src/containers/utility/AboutScreen.js
+++ src/containers/utility/AboutScreen.js
@@ -15,9 +15,12 @@ class AboutScreen extends Component {
                 title="VỀ ISOFH"
                 showFullScreen={true}>
                 <View style={{ padding: 15 }}>
-                    <Text>Nền tảng kết nối giữa người bệnh và bác sỹ</Text>
-                    <Text>Chạm để được chăm sóc, không phải mệt mỏi vì chờ đợi</Text>
-                    <Text>Quan tâm bản thân tốt hơn với hồ sơ y tế điện tử</Text>
+                    <Text>ISOFH được thành lập tháng 12/2015 với mục tiêu phát triển nền tảng hàng đầu về công nghệ thông tin y tế, xây dựng hệ thống y tế thông tin tại Việt Nam.</Text>
+                    <Text>3 giải pháp cốt lõi trong quản lý và vận hành hệ thống thông tin y tế cũng là thế mạnh nổi bật của ISOFH gồm:</Text>
+                    <Text style={{ marginTop: 10 }}>1. Giải pháp quản lý toàn diện</Text>
+                    <Text>2. Giải pháp kết nối hoàn chỉnh</Text>
+                    <Text>3. Giải pháp lưu trữ an toàn</Text>
+                    <Text style={{ marginTop: 10 }}>Đáp ứng hiệu quả: Tối ưu quy trình khám, chữa bệnh bằng công nghệ.</Text>
                 </View>
             </ActivityPanel>
         )
