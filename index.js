d830a15d2d5100beba88a79b2f33bd8db6e87be3
mainam <mainam.ctk33@gmail.com>
2019-04-17 11:35:25 +0700

    change packgate name

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import { AppRegistry } from 'react-native'
 import APP from './src'
-const server_url = 'http://api.test.appisofhcare.com:8382'; //dev
 AppRegistry.registerComponent('APP', () => APP);
