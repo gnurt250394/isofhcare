131ddd62d79818cf683c52975c10bfe48149929a
DinhHuan <huan.nd@isofh.com>
2019-05-16 09:01:27 +0700

    fix issuse ISOFHCARE-1999

--- src/data-access/hospital-provider.js
+++ src/data-access/hospital-provider.js
@@ -32,9 +32,10 @@ module.exports = {
             });
         });
     },
-    getBySearch(page,size){
+    getBySearch(page,size,stringQuyery){
+      let keyWord =   stringQuyery ? `stringQuyery=${stringQuyery}` : 'stringQuyery='
         return new Promise((resolve,reject) => {
-            client.requestApi("get", `${constants.api.hospital.get_hospital_by_search}?page=${page}&size=${size}&stringQuyery=&active=-1&type=-1`, {}, (s, e) => {
+            client.requestApi("get", `${constants.api.hospital.get_hospital_by_search}?page=${page}&size=${size}&${keyWord}&active=-1&type=-1`, {}, (s, e) => {
                 if (s)
                     resolve(s);
                 else
