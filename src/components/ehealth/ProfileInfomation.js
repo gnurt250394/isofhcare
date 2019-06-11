06d349bdd98bfe748008fa2bc2409a1ded1e18b4
mainam <mainam.ctk33@gmail.com>
2019-06-11 13:50:10 +0700

    show detail checkup result

--- src/components/ehealth/ProfileInfomation.js
+++ src/components/ehealth/ProfileInfomation.js
@@ -16,11 +16,9 @@ import ImageLoad from 'mainam-react-native-image-loader';
 import ScaledImage from "mainam-react-native-scaleimage";
 
 
-class SelectTimeScreen extends Component {
+class ProfileInfomation extends Component {
     constructor(props) {
         super(props);
-        debugger;
-        console.log(this.props.ehealth);
         this.state = {
             listTime: []
         }
@@ -28,11 +26,73 @@ class SelectTimeScreen extends Component {
 
 
     render() {
+        let { resultDetail } = this.props;
+        if (!resultDetail)
+            return null;
+        let serviceCheckup = (resultDetail.ListService || []).find(item => item.ServiceType == "CheckUp");
+        console.log(serviceCheckup);
+        console.log(this.props.ehealth);
         const icSupport = require("@images/new/user.png");
         const source = this.props.userApp.currentUser.avatar
             ? { uri: this.props.userApp.currentUser.avatar.absoluteUrl() }
             : icSupport;
-        return <View>
+        return <View style={{ flexDirection: 'row', position: 'relative', flex: 1, padding: 20 }}>
+            <View style={{ width: 2, top: 0, bottom: 0, left: 40, backgroundColor: '#8fa1aa' }}></View>
+            <View style={{ flex: 1, marginLeft: 26.5 }}>
+                <View style={[styles.item, { marginTop: 0 }]}>
+                    <View style={styles.round1}>
+                        <View style={styles.round2} />
+                    </View>
+                    <Text style={[styles.itemlabel, { fontWeight: 'bold', fontSize: 18, marginTop: 0 }]}>{this.props.ehealth.patient.patientName}</Text>
+                </View>
+                <View style={styles.item}>
+                    <View style={styles.round1}>
+                        <View style={styles.round3} />
+                    </View>
+                    <Text style={styles.itemlabel}>{this.props.ehealth.hospital.hospital.name}</Text>
+                </View>
+                <View style={styles.item}>
+                    <View style={styles.round1}>
+                        <View style={styles.round2} />
+                    </View>
+                    <Text style={styles.itemlabel}>Mã: <Text style={styles.itemcontent}>{this.props.ehealth.patient.patientHistoryId}</Text></Text>
+                </View>
+                {
+                    serviceCheckup &&
+                    <View>
+                        <View style={styles.item}>
+                            <View style={styles.round1}>
+                                <View style={styles.round3} />
+                            </View>
+                            <Text style={styles.itemlabel}>Dịch vụ: <Text style={styles.itemcontent}>{serviceCheckup.Name}</Text></Text>
+                        </View>
+                        <View style={styles.item}>
+                            <View style={styles.round1}>
+                                <View style={styles.round2} />
+                            </View>
+                            <Text style={styles.itemlabel}>Khoa: <Text style={styles.itemcontent}>{serviceCheckup.DepartmentName}</Text></Text>
+                        </View>
+                        <View style={styles.item}>
+                            <View style={styles.round1}>
+                                <View style={styles.round3} />
+                            </View>
+                            <Text style={styles.itemlabel}>Nơi khám: <Text style={styles.itemcontent}>{serviceCheckup.Location}</Text></Text>
+                        </View>
+                        <View style={styles.item}>
+                            <View style={styles.round1}>
+                                <View style={styles.round2} />
+                            </View>
+                            <Text style={styles.itemlabel}>Bác sĩ: <Text style={styles.itemcontent}>{serviceCheckup.DoctorFullName}</Text></Text>
+                        </View>
+                        <View style={styles.item}>
+                            <View style={styles.round1}>
+                                <View style={styles.round3} />
+                            </View>
+                            <Text style={styles.itemlabel}>Số khám: <Text style={styles.itemcontent}>{serviceCheckup.SequenceNoInt}</Text></Text>
+                        </View>
+                    </View>
+                }
+            </View>
             <ImageLoad
                 resizeMode="cover"
                 imageStyle={{ borderRadius: 35, borderWidth: 0.5, borderColor: 'rgba(151, 151, 151, 0.29)' }}
@@ -43,7 +103,7 @@ class SelectTimeScreen extends Component {
                     alignSelf: "center"
                 }}
                 placeholderSource={icSupport}
-                style={{ width: 70, height: 70, alignSelf: "center" }}
+                style={{ width: 70, height: 70 }}
                 resizeMode="cover"
                 loadingStyle={{ size: "small", color: "gray" }}
                 source={source}
@@ -53,24 +113,27 @@ class SelectTimeScreen extends Component {
                             resizeMode="cover"
                             source={icSupport}
                             width={70}
-                            style={{ width: 70, height: 70, alignSelf: "center" }}
+                            style={{ width: 70, height: 70 }}
                         />
                     );
                 }}
             />
-            <Text>header</Text>
         </View>
     }
 }
 
 function mapStateToProps(state) {
-    debugger;
     return {
         userApp: state.userApp,
         ehealth: state.ehealth
     };
 }
 const styles = StyleSheet.create({
-
+    round1: { width: 25, height: 25, backgroundColor: '#FFF', borderColor: '#8fa1aa', borderWidth: 2, borderRadius: 12.5, alignItems: 'center', justifyContent: 'center' },
+    round2: { width: 10, height: 10, borderRadius: 5, backgroundColor: '#7daa3c' },
+    round3: { width: 10, height: 10, borderRadius: 5, backgroundColor: '#c74444' },
+    itemlabel: { marginLeft: 5, flex: 1, marginTop: 2 },
+    itemcontent: { color: '#0076ff' },
+    item: { marginTop: 10, flexDirection: 'row' }
 })
-export default connect(mapStateToProps)(SelectTimeScreen);
+export default connect(mapStateToProps)(ProfileInfomation);
--- src/containers/ehealth/ViewEhealthDetailScreen.js
+++ src/containers/ehealth/ViewEhealthDetailScreen.js
@@ -16,6 +16,7 @@ import ImageLoad from 'mainam-react-native-image-loader';
 import { Calendar, LocaleConfig, Agenda } from 'react-native-calendars';
 import { Card } from 'native-base';
 import ProfileInfomation from '@components/ehealth/ProfileInfomation';
+import CheckupResult from '@components/ehealth/CheckupResult';
 
 const DEVICE_WIDTH = Dimensions.get('window').width;
 
@@ -24,7 +25,7 @@ class ViewEhealthDetailScreen extends Component {
         super(props)
         let result = this.props.navigation.state.params.result;
         let resultDetail = this.props.navigation.state.params.resultDetail;
-        debugger;
+        console.log(result);
         this.state = {
             result,
             resultDetail
@@ -47,124 +48,23 @@ class ViewEhealthDetailScreen extends Component {
                     color: '#FFF'
                 }}
                 isLoading={this.state.isLoading}>
-                <View style={{ flex: 1, alignItems: 'center' }}>
-                    <View style={{ height: 100 }}>
-                        <ScrollView ref={ref => this.flListDate = ref} horizontal={true} showsHorizontalScrollIndicator={false}>
-                            <ProfileInfomation />
-                        </ScrollView>
-                    </View>
-                </View>
+                <ScrollView ref={ref => this.flListDate = ref} showsVerticalScrollIndicator={false} style={{ flex: 1 }}>
+                    <ProfileInfomation resultDetail={this.state.resultDetail} />
+                    <View style={{ height: 1, backgroundColor: '#27ae60', }} />
+                    <CheckupResult result={this.state.result} />
+                </ScrollView>
             </ActivityPanel>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    card: {
-        borderRadius: 5,
-        backgroundColor: "#ffffff",
-        shadowColor: "rgba(0, 0, 0, 0.05)",
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowRadius: 10,
-        shadowOpacity: 1, marginTop: 10, padding: 10, flexDirection: 'row', shadowOpacity: 1, borderRadius: 8
-    },
-    style1: {
-        flexDirection: 'row', alignItems: 'center', marginTop: 10, marginLeft: 20
-    },
-    text1: {
-        fontSize: 16,
-        fontWeight: "bold",
-        fontStyle: "normal",
-        letterSpacing: 0,
-        color: "#000"
-    },
-    hospital_selected: {
-        alignItems: 'center',
-        height: 105,
-        width: 105,
-        backgroundColor: '#ffffff',
-        borderStyle: 'solid',
-        borderWidth: 3,
-        borderColor: '#02c39a',
-        borderRadius: 6,
-        margin: 5
-    },
-    hospital: {
-        alignItems: 'center',
-        height: 105,
-        width: 105,
-        backgroundColor: '#ffffff',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderColor: 'rgba(0, 0, 0, 0.15)',
-        borderRadius: 6,
-        margin: 5
-    },
-    item_ehealth: {
-        position: 'relative',
-        left: 20, right: 30
-    },
-    item_ehealth2: {
-        backgroundColor: '#f8fcf4',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderColor: 'rgba(155, 155, 155, 0.47)',
-        borderRadius: 6,
-        marginTop: 10,
-        marginLeft: 45,
-        padding: 12,
-    },
-    dash: { width: 2, flexDirection: 'column', position: 'absolute', top: 0, left: 10, bottom: 0 },
-    item_cycle: { width: 10, height: 10, backgroundColor: '#02c39a', borderRadius: 5, position: 'absolute', left: 6, top: '50%', marginTop: -5 },
-    hospital_text: { alignItems: 'flex-end', textAlign: 'center', margin: 5, fontSize: 13 },
-    avatar: {
-        alignSelf: 'center',
-        borderRadius: 25,
-        width: 45,
-        height: 45
-    },
-    viewBtn: {
-        width: 252,
-        maxWidth: DEVICE_WIDTH - 80,
-        height: 50,
-        borderRadius: 5,
-        marginVertical: 20,
-        backgroundColor: '#27AE60',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    cardView: {
-        marginTop: 20,
-        width: 350,
-        maxWidth: DEVICE_WIDTH - 50,
-        borderRadius: 5,
-
-        padding: 25,
-    },
-    viewLine: {
-        backgroundColor: '#4CD565',
-        height: '100%',
-        width: 1
-    },
-    viewBTnSuggest: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    btnReExamination: {
-        padding: 2, borderRadius: 3, marginRight: 5, marginVertical: 10, paddingHorizontal: 5
-    },
-    txLabel: {
-        color: '#9caac4',
-        fontSize: 15
-    },
-    txContent: {
-        color: '#554a4c',
-        marginTop: 5, marginBottom: 25,
-    }
+    round1: { width: 25, height: 25, backgroundColor: '#FFF', borderColor: '#8fa1aa', borderWidth: 2, borderRadius: 12.5, alignItems: 'center', justifyContent: 'center' },
+    round2: { width: 10, height: 10, borderRadius: 5, backgroundColor: '#7daa3c' },
+    round3: { width: 10, height: 10, borderRadius: 5, backgroundColor: '#c74444' },
+    itemlabel: { marginLeft: 5, flex: 1, marginTop: 2 },
+    itemcontent: { color: '#0076ff' },
+    item: { marginTop: 10, flexDirection: 'row' }
 });
 
 function mapStateToProps(state) {
