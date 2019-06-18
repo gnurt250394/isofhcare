c0f0ac6a3973e0731f3f6b9748d2199e5022f7b2
mainam <mainam.ctk33@gmail.com>
2019-06-18 14:23:48 +0700

    create shortcut file to deploy binary to appcenter

--- release-app.sh
+++ release-app.sh
@@ -1,4 +1,4 @@
-echo deploy to ISOFHCARE-ANDROID-USER-DEV
-appcenter codepush release-react -a isofhvn/ISOFHCARE-ANDROID-USER-DEV -d Production -m
-echo deploy to ISOFHCARE-IOS-USER-DEV
-appcenter codepush release-react -a isofhvn/ISOFHCARE-IOS-USER-DEV -d Production -m
+echo deploy to ISOFHCARE-ANDROID-USER-STABLE
+appcenter codepush release-react -a isofhvn/ISOFHCARE-ANDROID-USER-STABLE -d Production -m
+echo deploy to ISOFHCARE-IOS-USER-STABLE
+appcenter codepush release-react -a isofhvn/ISOFHCARE-IOS-USER-STABLE -d Production -m
