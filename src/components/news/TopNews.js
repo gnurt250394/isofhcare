e8442f8069178133c3a60e1ba34fef63be3f2747
mainam <mainam.ctk33@gmail.com>
2019-06-26 10:39:18 +0700

    init home page

---
+++ src/components/news/TopNews.js
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react';
+import { View, StyleSheet, } from 'react-native';
+
+class TopNews extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+   
+
+    render() {
+        return (
+            <View>
+                
+            </View >
+        );
+    }
+}
+const styles = StyleSheet.create({
+   
+});
+export default TopNews;
