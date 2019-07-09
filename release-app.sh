6a70888272133c231cd007e8ffded609e5a7fc42
mainam <mainam.ctk33@gmail.com>
2019-07-09 09:24:12 +0700

    config code push release branch

--- release-app.sh
+++ release-app.sh
@@ -1,4 +1,4 @@
 echo deploy to ISOFHCARE-ANDROID-USER-DEV
-appcenter codepush release-react -a isofhvn/ISOFHCARE-ANDROID-USER-DEV -d Production -m
+appcenter codepush release-react -a isofhvn/ISOFHCARE-ANDROID-USER-RELEASE -d Production -m
 echo deploy to ISOFHCARE-IOS-USER-DEV
-appcenter codepush release-react -a isofhvn/ISOFHCARE-IOS-USER-DEV -d Production -m
+appcenter codepush release-react -a isofhvn/ISOFHCARE-IOS-USER-RELEASE -d Production -m
