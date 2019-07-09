1fcd508708a159dd2bc4368d98512eb1df67574e
mainam <mainam.ctk33@gmail.com>
2019-07-09 08:27:04 +0700

    clear publish codepush

--- clear-deploy.sh
+++ clear-deploy.sh
@@ -1,2 +1,2 @@
-appcenter codepush deployment clear -a isofhvn/ISOFHCARE-ANDROID-USER-DEV Production
-appcenter codepush deployment clear -a isofhvn/ISOFHCARE-IOS-USER-DEV Production
+appcenter codepush deployment clear -a isofhvn/ISOFHCARE-ANDROID-USER-TEST Production
+appcenter codepush deployment clear -a isofhvn/ISOFHCARE-IOS-USER-TEST Production`
