3be4ac17157f201aff7ff328eb2efb652a15e155
mainam <mainam.ctk33@gmail.com>
2019-08-27 18:30:54 +0700

    remote command install nodejs

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -4,12 +4,12 @@
 # Installs version 1.5.3 which has no compatibility issues
 # 1.5.3 can be changed to another version you'd prefer
 
-echo running install node8
-set -ex
-brew uninstall node@6
-NODE_VERSION="8.9.4"
-curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
-sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+# echo running install node8
+# set -ex
+# brew uninstall node@6
+# NODE_VERSION="8.9.4"
+# curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
+# sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 
 echo running post-clone scripts
 echo uninstalling all cocoapods versions
