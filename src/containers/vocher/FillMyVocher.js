54dd8f47fadf61d74818e601ad9bd586723ccdfd
hoang trung <trung.hv@isofhcare.com>
2019-09-05 11:23:51 +0700

    cập nhật chức năng nhập mã voucher và xác nhận

--- src/containers/vocher/FillMyVocher.js
+++ src/containers/vocher/FillMyVocher.js
@@ -1,62 +1,73 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput ,Keyboard,KeyboardAvoidingView} from 'react-native';
 import TextField from "mainam-react-native-form-validate/TextField";
 import FloatingLabel from 'mainam-react-native-floating-label';
 import constants from "@resources/strings";
 import LinearGradient from 'react-native-linear-gradient'
 import Field from "mainam-react-native-form-validate/Field";
+import Form from "mainam-react-native-form-validate/Form";
 
 class FillMyVocher extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            voucher: ''
         };
     }
+    onChangeText = (s) => this.setState({ voucher: s })
 
+    comfirmVoucher=()=>{
+        Keyboard.dismiss();
+		if (!this.form.isValid()) {
+			return;
+        }
+       this.props.onPress && this.props.onPress(this.state.voucher)
+    }
     render() {
         return (
             <View style={styles.container}>
-                <TextField
-                    // getComponent={(value, onChangeText, onFocus, onBlur, isError) => <FloatingLabel
-                    //     placeholderStyle={{ fontSize: 16, fontWeight: '200' }} value={value} underlineColor={'#02C39A'}
-                    //     inputStyle={styles.textInputStyle}
-                    //     labelStyle={styles.labelStyle} placeholder={constants.phone} onChangeText={onChangeText} onBlur={onBlur} onFocus={onFocus} />}
-                    onChangeText={s => this.setState({ email: s })}
-                    inputStyle={styles.inputVoucher}
-                    errorStyle={styles.errorStyle}
-                    validate={{
-                        rules: {
-                            required: true,
-                        },
-                        messages: {
-                            required: "Số điện thoại không được bỏ trống",
-                        }
-                    }}
-
-                    placeholder={'Mã ưu đãi'}
-                    autoCapitalize={"none"}
-                />
+                <KeyboardAvoidingView behavior="padding">
+                <Form ref={ref => (this.form = ref)}>
+                        <TextField
+                            onChangeText={this.onChangeText}
+                            inputStyle={styles.inputVoucher}
+                            errorStyle={styles.errorStyle}
+                            validate={{
+                                rules: {
+                                    required: true,
+                                },
+                                messages: {
+                                    required: "Mã ưu đãi không được bỏ trống",
+                                }
+                            }}
+                            placeholder={'Mã ưu đãi'}
+                            autoCapitalize={"none"}
+                        />
+                </Form>
                 <LinearGradient
                     colors={['rgb(255, 214, 51)', 'rgb(204, 163, 0)', 'rgb(179, 143, 0)']}
                     locations={[0, 0.7, 1]}
                     style={styles.containerButton}>
                     <TouchableOpacity
-                        style={{
-                            flex: 1,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            paddingVertical: 8,
-
-                        }}
+                    onPress={this.comfirmVoucher}
+                        style={styles.button}
                     >
                         <Text style={styles.txtButton}>XÁC NHẬN</Text>
                     </TouchableOpacity>
                 </LinearGradient>
+                </KeyboardAvoidingView>
             </View>
         );
     }
 }
 const styles = StyleSheet.create({
+    button: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 8,
+
+    },
     txtButton: {
         color: '#fff',
         fontSize: 16,
