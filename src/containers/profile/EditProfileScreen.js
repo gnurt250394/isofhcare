f37547e6fb3304c78f7ef27fdd4111ba1d57707a
DinhHuan <huan.nd@isofh.com>
2019-09-14 09:30:34 +0700

    fix address user profile info

--- src/containers/profile/EditProfileScreen.js
+++ src/containers/profile/EditProfileScreen.js
@@ -300,7 +300,7 @@ class EditProfileScreen extends Component {
                             "provinceId": this.state.provinces ? this.state.provinces.id.toString() : null,
                             "districtId": this.state.districts ? this.state.districts.id.toString() : null,
                             "zoneId": this.state.zone ? this.state.zone.id.toString() : null,
-                            "village": this.state.address ? this.state.address : ' ',
+                            "village": this.state.address ? this.state.address : '',
                             "relationshipType": this.state.relationShip && this.state.relationShip.type ? this.state.relationShip.type : (this.state.relationshipType || null)
                         }
                         profileProvider.updateProfile(id, data).then(res => {
