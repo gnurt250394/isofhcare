066107f5a4ec5e0eee5679c128a397846cb6053a
hoang trung <trung.hv@isofhcare.com>
2019-12-28 11:35:29 +0700

    update Api list doctor with specialist

--- src/data-access/booking-doctor-provider.js
+++ src/data-access/booking-doctor-provider.js
@@ -18,6 +18,37 @@ module.exports = {
             );
         });
     },
+    getListDoctorWithSpecialist(idSpecialist, page, size) {
+        return new Promise((resolve, reject) => {
+
+            client.requestApi(
+                "get",
+                client.serviceSchedule +
+                constants.api.booking.doctor.get_detail_doctor + `/${idSpecialist}/specialization?page=${page}&size=${size}&sort=desc&properties=created`,
+                {},
+                (s, e) => {
+                    if (s) resolve(s);
+                    reject(e);
+                }
+            );
+        });
+    },
+    getListHospitalWithSpecialist(idSpecialist, page, size) {
+        return new Promise((resolve, reject) => {
+
+            client.requestApi(
+                "get",
+                client.serviceSchedule +
+                constants.api.booking.doctor.get_detail_hospital + `/${idSpecialist}/specialization?page=${page}&size=${size}&sort=desc&properties=created`,
+                {},
+                (s, e) => {
+                    if (s) resolve(s);
+                    reject(e);
+                }
+            );
+        });
+    },
+
     detailDoctor(id) {
         return new Promise((resolve, reject) => {
             client.requestApi(
@@ -79,7 +110,7 @@ module.exports = {
      * @param {string} time 
      * @param {object} room 
      */
-    create(date, description, doctor, hospitals, items, patient, scheduleId, time, room, idUser,images) {
+    create(date, description, doctor, hospitals, items, patient, scheduleId, time, room, idUser, images) {
         console.log('hospitals: ', hospitals);
         return new Promise((resolve, reject) => {
             let doctors = { id: doctor.id, name: doctor.name }
@@ -182,7 +213,7 @@ module.exports = {
             client.requestApi(
                 "get",
                 client.serviceSchedule +
-                `${constants.api.booking.doctor.get_list_specialists}?page=${page}&size=${size}`
+                `${constants.api.booking.doctor.get_list_specialists}?page=${page}&size=${size}&sort=desc&properties=created`
                 , {}, (s, e) => {
                     if (s) resolve(s);
                     else reject(e);
