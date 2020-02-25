5685093dcfb7bddeea7be4e80e6a7795fe3cc57c
huannd69 <huannd08@gmail.com>
2020-02-25 18:15:28 +0700

    change api stable

--- src/data-access/service-provider.js
+++ src/data-access/service-provider.js
@@ -2,7 +2,7 @@ import client from '@utils/client-utils';
 import string from 'mainam-react-native-string-utils';
 import constants from '@resources/strings';
 import datacacheProvider from '@data-access/datacache-provider';
-const URL = 'http://10.0.0.98:8080/'
+const URL = 'http://10.0.0.88:8088/'
 module.exports = {
     getAll(hospitalId, specialistId, serviceTypeId, requestApi) {
         return new Promise((resolve, reject) => {
