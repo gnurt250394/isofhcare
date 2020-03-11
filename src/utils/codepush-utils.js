7bd3d012740ab7d29c2df59fa7522585bd32cf6a
trunghv <trung.hv@isofhcare.com>
2020-03-11 11:55:20 +0700

    add linking to app store

--- src/utils/codepush-utils.js
+++ src/utils/codepush-utils.js
@@ -1,10 +1,33 @@
 import codePush from "react-native-code-push";
-import { Alert } from 'react-native';
+import { Alert, Linking, Platform } from 'react-native';
 import snackbar from "@utils/snackbar-utils";
 import constants from '@resources/strings';
 import dataCacheProvider from '@data-access/datacache-provider';
+function updateFromAppStore() {
+    Alert.alert(
+        'THÔNG BÁO',
+        'Ứng dụng đã có phiên bản mới. Bạn vui lòng cập nhật để có trải nghiệm tốt nhất!',
+        [
+            {
+                text: 'Cập nhật', onPress: () => {
+                    let link = Platform.OS == 'android'
+                        ? 'market://details?id=com.isofh.isofhcare'
+                        : `itms-apps://itunes.apple.com/us/app/id1428148423?mt=8`
+                    Linking.canOpenURL(link).then(supported => {
+                        supported && Linking.openURL(link);
+                    }, (err) => {
+                        console.log('err: ', err);
+
+                    })
+                }
+            },
+        ],
+        { cancelable: false })
+}
 module.exports = {
     checkupDate(silent) {
+        // updateFromAppStore()
+        // return
         codePush.checkForUpdate().then(update => {
             if (update) {
                 if (update.isMandatory) {
