c8cdf2405408c559ec105c98353022ca8718e960
trung.hv <trung.hv@isofhcare.com>
2020-06-01 17:53:02 +0700

    fix bug ehealth

--- android/app/src/main/java/com/isofh/appisofhcarestable/MainApplication.java
+++ android/app/src/main/java/com/isofh/appisofhcarestable/MainApplication.java
@@ -65,11 +65,10 @@ import com.microsoft.codepush.react.CodePush;
     SoLoader.init(this, /* native exopackage */ false);
   }
 
-  @Override
-  public String getFileProviderAuthority() {
-         return "com.isofh.appisofhcarestable.provider";
-  }
-
+ @Override
+     public String getFileProviderAuthority() {
+            return BuildConfig.APPLICATION_ID + ".provider";
+     }
   // @Override
   // protected void attachBaseContext(Context base) {
   //   super.attachBaseContext(base);
