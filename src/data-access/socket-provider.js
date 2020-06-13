8e59f67b8cd4e0f7f8d22f88c6baedb932fd65bf
trung.hv <trung.hv@isofhcare.com>
2020-06-13 09:52:21 +0700

    config webrtc video call

---
+++ src/data-access/socket-provider.js
@@ -0,0 +1,39 @@
+import io from 'socket.io-client';
+const connectSocket = async (token) => {
+    let socket = null
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (token) {
+                console.log('token: ', token);
+                // const url = 'http://192.168.1.5:4443';
+                // const url = 'http://192.168.43.31:4443';
+                const url = 'http://10.0.50.212:4443';
+                // const url = 'https://node-js-webbrtc-server.herokuapp.com';
+                socket = io.connect(url, {
+                    transports: ['websocket'], query: {
+                        token: token,
+                    },
+                    upgrade: true,
+                    reconnection: true,
+                    autoConnect: true,
+                    timeout: 30000,
+                    rememberUpgrade: true
+                })
+                if (!socket.connected) {
+                    socket.connect()
+                }
+                resolve(socket)
+
+            }else{
+                reject()
+            }
+        } catch (error) {
+            console.log('error: ', error);
+            reject()
+        }
+    })
+}
+
+export default {
+    connectSocket
+}
