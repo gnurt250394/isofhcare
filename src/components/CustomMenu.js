4288fe604fcbf0967c1d2a83613b4803c6c9bdab
trung.hv <trung.hv@isofhcare.com>
2020-07-03 11:57:04 +0700

    add ui chat

---
+++ src/components/CustomMenu.js
@@ -0,0 +1,66 @@
+import React, {memo} from 'react';
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+  renderers,
+} from 'react-native-popup-menu';
+import {View, Text, StyleSheet} from 'react-native';
+const {Popover, ContextMenu, NotAnimatedContextMenu, SlideInMenu} = renderers;
+
+const CustomMenu = ({
+  MenuSelectOption,
+  options,
+  customOption,
+  onSelected,
+  ...props
+}) => {
+  const onSelect = (e, i) => () => {
+    onSelected(e, i);
+  };
+  return (
+    <Menu
+      {...props}
+      renderer={Popover}
+      rendererProps={{
+        placement: 'bottom',
+        anchorStyle: {backgroundColor: '#00CBA7'},
+      }}>
+      <MenuTrigger>{MenuSelectOption}</MenuTrigger>
+      <MenuOptions>
+        {customOption
+          ? customOption
+          : options?.length
+          ? options.map((e, i) => {
+              return (
+                <MenuOption
+                  key={i}
+                  style={styles.containerOption}
+                  onSelect={onSelect(e, i)}>
+                  <View style={[styles.buttonAnwser]}>
+                    <Text>{e.value}</Text>
+                  </View>
+                </MenuOption>
+              );
+            })
+          : null}
+      </MenuOptions>
+    </Menu>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerOption: {
+    borderBottomColor: '#00000020',
+    borderBottomWidth: 1,
+  },
+  buttonAnwser: {
+    // alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 10,
+  },
+});
+export default memo(CustomMenu);
---
+++ src/components/community/RenderPlaceHolder.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {
+  Placeholder,
+  PlaceholderMedia,
+  PlaceholderLine,
+  Fade,
+  ShineOverlay,
+} from 'rn-placeholder';
+import {Card} from 'native-base';
+const RenderPlaceHolder = ({data = [...new Array(10).fill({})]}) => {
+  return data.map((e, i) => {
+    return (
+      <Card key={i} style={styles.card}>
+        <Placeholder Animation={ShineOverlay} style={styles.placeContainer}>
+          <View style={styles.row}>
+            <PlaceholderMedia style={[styles.placeMedia]} />
+            <View style={{flex: 1, paddingLeft: 5}}>
+              <PlaceholderLine width={30} height={10} />
+              <PlaceholderLine width={15} height={10} />
+            </View>
+          </View>
+          <PlaceholderLine width={95} />
+          <PlaceholderLine width={95} />
+          <PlaceholderLine width={50} />
+        </Placeholder>
+      </Card>
+    );
+  });
+};
+
+export default RenderPlaceHolder;
+
+const styles = StyleSheet.create({
+  card: {
+    paddingVertical: 10,
+    paddingRight: 10,
+  },
+  placeContainer: {
+    marginTop: 10,
+    marginHorizontal: 15,
+    borderRadius: 4,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    paddingBottom: 10,
+  },
+  placeMedia: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+});
