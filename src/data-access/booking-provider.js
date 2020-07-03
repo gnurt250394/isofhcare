ad5776da71b4ddc7a367052a0c57ae0c0c0e7ec0
trung.hv <trung.hv@isofhcare.com>
2020-07-03 13:48:42 +0700

    .

--- src/data-access/booking-provider.js
+++ src/data-access/booking-provider.js
@@ -1,81 +1,83 @@
-import client from "@utils/client-utils";
-import string from "mainam-react-native-string-utils";
-import constants from "@resources/strings";
+import client from '@utils/client-utils';
+import string from 'mainam-react-native-string-utils';
+import constants from '@resources/strings';
 
 export default {
   getListBooking(profileId, hospitalId) {
     return new Promise((resolve, reject) => {
       if (hospitalId)
         client.requestApi(
-          "get",
+          'get',
           constants.api.booking.get_list_booking +
-          "/" +
-          profileId +
-          "?hospitalId=" +
-          hospitalId,
+            '/' +
+            profileId +
+            '?hospitalId=' +
+            hospitalId,
           {},
           (s, e) => {
             if (s) resolve(s);
             reject(e);
-          }
+          },
         );
       else {
         client.requestApi(
-          "get",
+          'get',
           constants.api.booking.get_list_booking,
           {},
           (s, e) => {
             if (s) resolve(s);
             reject(e);
-          }
+          },
         );
       }
     });
   },
   detailPatientHistory(patientHistoryId, hospitalId, id, shareId) {
-    let id2 = id ? `&id=${id}` : ''
-    let shareId2 = shareId ? `&shareId=${shareId}` : ''
+    let id2 = id ? `&id=${id}` : '';
+    let shareId2 = shareId ? `&shareId=${shareId}` : '';
     return new Promise((resolve, reject) => {
       client.requestApi(
-        "get",
+        'get',
         constants.api.booking.get_detail_patient_historyid +
-        "/" +
-        patientHistoryId +
-        "?hospitalId=" +
-        hospitalId + id2 + shareId2,
+          '/' +
+          patientHistoryId +
+          '?hospitalId=' +
+          hospitalId +
+          id2 +
+          shareId2,
         {},
         (s, e) => {
           if (s) resolve(s);
           else reject(e);
-        }
+        },
       );
     });
   },
   resultPatientHistory(patientHistoryId, source) {
     return new Promise((resolve, reject) => {
       client.requestApi(
-        "get",
+        'get',
         constants.api.booking.get_result_patient_historyid +
-        "/" +
-        patientHistoryId +
-        "?hospitalId=" +
-        source,
+          '/' +
+          patientHistoryId +
+          '?hospitalId=' +
+          source,
         {},
         (s, e) => {
           if (s) resolve(s);
           else reject(e);
-        }
+        },
       );
     });
   },
   delete(bookingId, source, callback) {
     client.requestApi(
-      "delete",
-      constants.api.booking.delete + "/" + bookingId + "?source=" + source,
+      'delete',
+      constants.api.booking.delete + '/' + bookingId + '?source=' + source,
       {},
       (s, e) => {
         if (callback) callback(s, e);
-      }
+      },
     );
   },
   getByAuthor() {
@@ -89,26 +91,41 @@ export default {
       // var fromDate = '1970-01-01 00:00:00'
 
       client.requestApi(
-        "get",
-        `${constants.api.booking.getByAuthor}`, {}, (s, e) => {
+        'get',
+        `${constants.api.booking.getByAuthor}`,
+        {},
+        (s, e) => {
           if (s) resolve(s);
           else reject(e);
-        }
+        },
       );
     });
   },
   detail(id) {
     return new Promise((resolve, reject) => {
-      client.requestApi('get', `${constants.api.booking.detail}/${id}`, {}, (s, e) => {
-        if (s) resolve(s)
-        else reject(e)
-      })
-    })
+      client.requestApi(
+        'get',
+        `${constants.api.booking.detail}/${id}`,
+        {},
+        (s, e) => {
+          if (s) resolve(s);
+          else reject(e);
+        },
+      );
+    });
   },
-  create(hospitalId, detailScheduleId, medicalRecordId, serviceTypeId, serviceId, bookingTime, content) {
+  create(
+    hospitalId,
+    detailScheduleId,
+    medicalRecordId,
+    serviceTypeId,
+    serviceId,
+    bookingTime,
+    content,
+  ) {
     return new Promise((resolve, reject) => {
       client.requestApi(
-        "post",
+        'post',
         `${constants.api.booking.create}/${hospitalId}`,
         {
           detailScheduleId,
@@ -118,60 +135,97 @@ export default {
           booking: {
             bookingTime,
             content,
-          }
-        }, (s, e) => {
+          },
+        },
+        (s, e) => {
           if (s) resolve(s);
           else reject(e);
-        }
+        },
       );
     });
   },
   confirmPayment(bookingId, paymentMethod) {
     return new Promise((resolve, reject) => {
-      let body = { pay: paymentMethod }
+      let body = {pay: paymentMethod};
       client.requestApi(
-        "put",
-        `${constants.api.booking.confirmPay}/${bookingId}`
-        , body, (s, e) => {
+        'put',
+        `${constants.api.booking.confirmPay}/${bookingId}`,
+        body,
+        (s, e) => {
           if (s) resolve(s);
           else reject(e);
-        }
+        },
       );
     });
   },
   /**
-     * 
-     * @param {string} date 
-     * @param {string} description 
-     * @param {object} hospital 
-     * @param {object} items 
-     * @param {object} patient 
-     * @param {string} payment 
-     * @param {string} time 
-     * @param {object} room 
-     * @param {array} images 
-     */
+   *
+   * @param {string} date
+   * @param {string} description
+   * @param {object} hospital
+   * @param {object} items
+   * @param {object} patient
+   * @param {string} payment
+   * @param {string} time
+   * @param {object} room
+   * @param {array} images
+   */
 
-  createBooking(date, description, hospitals, items, patientUser, time, idUser, images) {
+  createBooking(
+    date,
+    description,
+    hospitals,
+    items,
+    patientUser,
+    time,
+    idUser,
+    images,
+  ) {
     console.log('items: ', items);
     return new Promise((resolve, reject) => {
-      let hospital = { id: hospitals && hospitals.id || '', name: hospitals && hospitals.name || '', address: hospitals && hospitals.address || hospitals.contact && hospitals.contact.address || '', checkInPlace: hospitals && hospitals.checkInPlace || '', hotLine: hospitals && hospitals.hotLine || '', bank: hospitals && hospitals.transferInfo && hospitals.transferInfo.bank || '', accountNo: hospitals && hospitals.transferInfo && hospitals.transferInfo.accountNo || '', owner: hospitals && hospitals.transferInfo && hospitals.transferInfo.owner || '', branch: hospitals && hospitals.transferInfo && hospitals.transferInfo.branch || '', note: hospitals && hospitals.transferInfo && hospitals.transferInfo.note || ''  }
+      let hospital = {
+        id: (hospitals && hospitals.id) || '',
+        name: (hospitals && hospitals.name) || '',
+        address:
+          (hospitals && hospitals.address) ||
+          (hospitals.contact && hospitals.address) ||
+          '',
+        checkInPlace: (hospitals && hospitals.checkInPlace) || '',
+        hotLine:
+          (hospitals && (hospitals.hotLine || hospitals?.contact?.hotLine)) || '',
+        bank:
+          (hospitals && (hospitals.bank || hospitals?.transferInfo?.bank)) ||
+          '',
+        accountNo:
+          (hospitals &&
+            (hospitals.accountNo || hospitals?.transferInfo?.accountNo)) ||
+          '',
+        owner:
+          (hospitals && (hospitals.owner || hospitals?.transferInfo?.owner)) ||
+          '',
+        branch:
+          (hospitals &&
+            (hospitals.branch || hospitals?.transferInfo?.branch)) ||
+          '',
+        note:
+          (hospitals && (hospitals.note || hospitals?.transferInfo?.note)) ||
+          '',
+      };
       console.log('hospital: ', hospitals);
       let patient = {
         id: idUser,
         name: patientUser.name,
-        phone: patientUser.phone
-      }
+        phone: patientUser.phone,
+      };
       client.requestApi(
-        "post",
-        client.serviceBooking +
-        constants.api.booking.create_booking,
+        'post',
+        client.serviceBooking + constants.api.booking.create_booking,
         {
           // ngày đặt khám
           date,
           // mô tả
           description,
-          // thông tin bệnh viện đặt khám 
+          // thông tin bệnh viện đặt khám
           hospital,
           // danh sách dịch vụ
           items,
@@ -181,11 +235,12 @@ export default {
           time,
           //owner : true: đặt khám chính chủ, false: đặt khám hộ
           owner: patientUser.status == 1 ? true : false,
-          images
-        }, (s, e) => {
+          images,
+        },
+        (s, e) => {
           if (s) resolve(s);
           else reject(e);
-        }
+        },
       );
     });
   },
