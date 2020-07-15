7569ac29e26ec13f4c871a31bc3752dbfa62b1bc
trung.hv <trung.hv@isofhcare.com>
2020-07-15 09:25:32 +0700

    add auto update app
    (cherry picked from commit bea52a58d4319f96a05401d3c0ec52973064e928)

--- release-app.sh
+++ release-app.sh
@@ -1,4 +1,9 @@
+# run: appcenter codepush release-react -a isofhvn/ISOFHCARE-ANDROID-USER-STABLE -d Production -m 
+# nếu bản update là bắt buộc phải cập nhật đến người dùng ngay khi mở app.
+# 
 echo deploy to ISOFHCARE-ANDROID-USER-STABLE
-appcenter codepush release-react -a isofhvn/ISOFHCARE-ANDROID-USER-STABLE -d Production -m
+appcenter codepush release-react -a isofhvn/ISOFHCARE-ANDROID-USER-STABLE -d Production
+
+# run: appcenter codepush release-react -a isofhvn/ISOFHCARE-IOS-USER-STABLE -d Production -m 
 echo deploy to ISOFHCARE-IOS-USER-STABLE
-appcenter codepush release-react -a isofhvn/ISOFHCARE-IOS-USER-STABLE -d Production -m
+appcenter codepush release-react -a isofhvn/ISOFHCARE-IOS-USER-STABLE -d Production
