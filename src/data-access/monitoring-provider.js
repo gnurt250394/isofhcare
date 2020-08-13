bd41c8bd17a701a9454dff100f0c48a0be33df60
trunghv <gnurt250394@gmail.com>
2020-08-13 11:35:31 +0700

    .

---
+++ src/data-access/monitoring-provider.js
@@ -0,0 +1,97 @@
+import client from '@utils/client-utils';
+import string from 'mainam-react-native-string-utils';
+import constants from '@resources/strings';
+
+export default {
+  createHeightWeight(date, height, weight) {
+    let params = {};
+    if (date) params.date = date;
+    if (height) params.height = Number(height);
+    if (weight) params.weight = Number(weight);
+    return new Promise((resolve, reject) => {
+      client.requestApi(
+        'post',
+        constants.api.monitoring.height_weight,
+        params,
+        (s, e) => {
+          if (s) resolve(s);
+          else reject(e);
+        },
+      );
+    });
+  },
+
+  createBodyTemperature(date, bodyTemperature) {
+    let params = {};
+    if (date) params.date = date;
+    if (bodyTemperature) params.bodyTemperature = Number(bodyTemperature);
+    return new Promise((resolve, reject) => {
+      client.requestApi(
+        'post',
+        constants.api.monitoring.body_temperature,
+        params,
+        (s, e) => {
+          if (s) resolve(s);
+          else reject(e);
+        },
+      );
+    });
+  },
+
+  createBloodPressure(date, diastolic, systolic) {
+    let params = {};
+    if (date) params.date = date;
+    if (systolic) params.systolic = Number(systolic);
+    if (diastolic) params.diastolic = Number(diastolic);
+    return new Promise((resolve, reject) => {
+      client.requestApi(
+        'post',
+        constants.api.monitoring.blood_pressure,
+        params,
+        (s, e) => {
+          if (s) resolve(s);
+          else reject(e);
+        },
+      );
+    });
+  },
+  getHeightWeight(page, size) {
+    return new Promise((resolve, reject) => {
+      client.requestApi(
+        'get',
+        constants.api.monitoring.height_weight + `?page=${page}&size=${size}`,
+        {},
+        (s, e) => {
+          if (s) resolve(s);
+          else reject(e);
+        },
+      );
+    });
+  },
+  getBloodPressure(page, size) {
+    return new Promise((resolve, reject) => {
+      client.requestApi(
+        'get',
+        constants.api.monitoring.blood_pressure + `?page=${page}&size=${size}`,
+        {},
+        (s, e) => {
+          if (s) resolve(s);
+          else reject(e);
+        },
+      );
+    });
+  },
+  getBodyTemperature(page, size) {
+    return new Promise((resolve, reject) => {
+      client.requestApi(
+        'get',
+        constants.api.monitoring.body_temperature + `?page=${page}&size=${size}`,
+        {},
+        (s, e) => {
+          if (s) resolve(s);
+          else reject(e);
+        },
+      );
+    });
+  },
+};
--- src/utils/object-utils.js
+++ src/utils/object-utils.js
@@ -36,4 +36,70 @@ export default {
         return '';
     }
   },
+
+};
+String.prototype.format = function(format) {
+
+    var self = this;
+    var half = false;
+
+    if (format && format[0] === '!') {
+        half = true;
+        format = format.substring(1);
+    }
+
+    if (format === undefined || format === null || format === '')
+        return self.getFullYear() + '-' + (self.getMonth() + 1).toString().padLeft(2, '0') + '-' + self.getDate().toString().padLeft(2, '0') + 'T' + self.getHours().toString().padLeft(2, '0') + ':' + self.getMinutes().toString().padLeft(2, '0') + ':' + self.getSeconds().toString().padLeft(2, '0') + '.' + self.getMilliseconds().toString().padLeft(3, '0') + 'Z';
+
+    var h = self.getHours();
+
+    if (half) {
+        if (h >= 12)
+            h -= 12;
+    }
+
+    return format.replace(regexpDATEFORMAT, function(key) {
+        switch (key) {
+            case 'yyyy':
+                return self.getFullYear();
+            case 'yy':
+                return self.getYear();
+            case 'MM':
+                return (self.getMonth() + 1).toString().padLeft(2, '0');
+            case 'M':
+                return (self.getMonth() + 1);
+            case 'dd':
+                return self.getDate().toString().padLeft(2, '0');
+            case 'd':
+                return self.getDate();
+            case 'HH':
+            case 'hh':
+                return h.toString().padLeft(2, '0');
+            case 'H':
+            case 'h':
+                return self.getHours();
+            case 'mm':
+                return self.getMinutes().toString().padLeft(2, '0');
+            case 'm':
+                return self.getMinutes();
+            case 'ss':
+                return self.getSeconds().toString().padLeft(2, '0');
+            case 's':
+                return self.getSeconds();
+            case 'w':
+            case 'ww':
+                var tmp = new Date(+self);
+                tmp.setHours(0, 0, 0);
+                tmp.setDate(tmp.getDate() + 4 - (tmp.getDay() || 7));
+                tmp = Math.ceil((((tmp - new Date(tmp.getFullYear(), 0, 1)) / 8.64e7) + 1) / 7);
+                if (key === 'ww')
+                    return tmp.toString().padLeft(2, '0');
+                return tmp;
+            case 'a':
+                var a = 'AM';
+                if (self.getHours() >= 12)
+                    a = 'PM';
+                return a;
+        }
+    });
 };
