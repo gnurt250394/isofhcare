1f09efd0485c4768449926988ed1b06902d26048
trunghv <gnurt250394@gmail.com>
2020-08-20 09:49:32 +0700

    upload file

--- src/data-access/image-provider.js
+++ src/data-access/image-provider.js
@@ -1,28 +1,39 @@
-
 import client from '@utils/client-utils';
 import constants from '@resources/strings';
 
-module.exports = {
-    upload(uri, type, callback) {
-
-        if (!callback) {
-            return new Promise((resolve, reject) => {
-                client.uploadFile(constants.api.upload.image, uri, type, (s, e) => {
-                    if (s)
-                        resolve({ data: s, uri, success: true });
-                    resolve({ uri, success: false });
-                });
-            });
+export default {
+  upload(uri, type, callback) {
+    if (!callback) {
+      return new Promise((resolve, reject) => {
+        client.uploadFile(constants.api.upload.image, uri, type, (s, e) => {
+          if (s) resolve({data: s, uri, success: true});
+          resolve({uri, success: false});
+        });
+      });
+    } else
+      client.uploadFile(constants.api.upload.image, uri, type, (s, e) => {
+        if (callback) {
+          if (s) {
+            callback({data: s, uri, success: true}, e);
+          } else callback({uri, success: false}, e);
         }
-        else
-            client.uploadFile(constants.api.upload.image, uri, type, (s, e) => {
-                if (callback) {
-                    if (s) {
-                        callback({ data: s, uri, success: true }, e);
-                    }
-                    else
-                        callback({ uri, success: false }, e);
-                }
-            });
-    }
-}
+      });
+  },
+  uploadFile(uri, type, callback) {
+    if (!callback) {
+      return new Promise((resolve, reject) => {
+        client.uploadFile(constants.api.upload.file, uri, type, (s, e) => {
+          if (s) resolve({data: s, uri, success: true});
+          resolve({uri, success: false});
+        });
+      });
+    } else
+      client.uploadFile(constants.api.upload.file, uri, type, (s, e) => {
+        if (callback) {
+          if (s) {
+            callback({data: s, uri, success: true}, e);
+          } else callback({uri, success: false}, e);
+        }
+      });
+  },
+};
