d4a72d8a145728063a4ab492eaf7cbed030733b4
trunghv <gnurt250394@gmail.com>
2020-09-03 14:31:41 +0700

    config reactotron

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native'
-import APP from './src'
+import {AppRegistry} from 'react-native';
+import APP from './src';
 import bgMessage from '@components/bgMessage';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 if (process.env.NODE_ENV !== 'development') {
-    console.log = () => { }
-    console.warn = () => { }
-    console.error = () => { }
+  console.log = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
+if (__DEV__) {
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
 }
 AppRegistry.registerComponent(appName, () => APP);
-AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessage); 
+AppRegistry.registerHeadlessTask(
+  'RNFirebaseBackgroundMessage',
+  () => bgMessage,
+);
