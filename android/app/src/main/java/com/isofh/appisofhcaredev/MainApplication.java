bddc3e71d2ef20ec547e4e34c64a5ecdd074a322
trunghv <gnurt250394@gmail.com>
2020-09-14 10:08:37 +0700

    .

--- android/app/src/main/java/com/isofh/appisofhcaredev/MainApplication.java
+++ android/app/src/main/java/com/isofh/appisofhcaredev/MainApplication.java
@@ -15,7 +15,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.isofh.appisofhcaretest.LaunchApplicationPackage;
+import com.isofh.appisofhcaredev.LaunchApplicationPackage;
 import com.microsoft.codepush.react.ReactInstanceHolder;
 import com.microsoft.codepush.react.CodePush;
 // public class MainApplication extends MultiDexApplication implements ReactApplication, ShareApplication {
@@ -43,7 +43,6 @@ import com.microsoft.codepush.react.CodePush;
       // Packages that cannot be autolinked yet can be added manually here, for
       // example:
       // packages.add(new RNCViewPagerPackage());
-      packages.add(new RNFingerprintChangePackage());
       packages.add(new RNFirebaseNotificationsPackage());
       packages.add(new RNFirebaseMessagingPackage());
       packages.add(new LaunchApplicationPackage());
