7e9e259484e2de25708f40d294f08a8c2384a63d
trunghv <gnurt250394@gmail.com>
2021-01-13 17:39:32 +0700

    App user_Ghép API phương thức thanh toán của CSYT

--- src/data-access/payment-provider.js
+++ src/data-access/payment-provider.js
@@ -3,34 +3,57 @@ import constants from '@resources/strings';
 import datacacheProvider from '@data-access/datacache-provider';
 
 export default {
-    getListCard() {
-        return new Promise((resolve, reject) => {
-            client.requestApi('get', client.requestPayment + constants.api.payment.get_list_card, {}, (s, e) => {
-                if (s)
-                    resolve(s)
-                if (e)
-                    reject(e)
-            })
-        })
-    },
-    createNewCard() {
-        return new Promise((resolve, reject) => {
-            client.requestApi('post', client.requestPayment + constants.api.payment.create_new_card, {}, (s, e) => {
-                if (s)
-                    resolve(s)
-                if (e)
-                    reject(e)
-            })
-        })
-    },
-    deleteCard(id) {
-        return new Promise((resolve, reject) => {
-            client.requestApi('delete', client.requestPayment + constants.api.payment.get_list_card + '/' + id, {}, (s, e) => {
-                if (s)
-                    resolve(s)
-                if (e)
-                    reject(e)
-            })
-        })
-    },
-}
+  getListCard() {
+    return new Promise((resolve, reject) => {
+      client.requestApi(
+        'get',
+        client.requestPayment + constants.api.payment.get_list_card,
+        {},
+        (s, e) => {
+          if (s) resolve(s);
+          if (e) reject(e);
+        },
+      );
+    });
+  },
+  getListPayment(idHospital) {
+    return new Promise((resolve, reject) => {
+      client.requestApi(
+        'get',
+        constants.api.hospital.get_list_payment +
+          `${idHospital}/payment-methods`,
+        {},
+        (s, e) => {
+          if (s) resolve(s);
+          if (e) reject(e);
+        },
+      );
+    });
+  },
+  createNewCard() {
+    return new Promise((resolve, reject) => {
+      client.requestApi(
+        'post',
+        client.requestPayment + constants.api.payment.create_new_card,
+        {},
+        (s, e) => {
+          if (s) resolve(s);
+          if (e) reject(e);
+        },
+      );
+    });
+  },
+  deleteCard(id) {
+    return new Promise((resolve, reject) => {
+      client.requestApi(
+        'delete',
+        client.requestPayment + constants.api.payment.get_list_card + '/' + id,
+        {},
+        (s, e) => {
+          if (s) resolve(s);
+          if (e) reject(e);
+        },
+      );
+    });
+  },
+};
