f1794e30a7e939bb142410d64b80c60325d52938
trunghv <gnurt250394@gmail.com>
2021-04-03 08:21:48 +0700

    .

--- clear-deploy.sh
+++ clear-deploy.sh
@@ -1,2 +1,2 @@
-appcenter codepush deployment clear -a isofhvn/ISOFHCARE-ANDROID-USER-PRODUCTION Production
-appcenter codepush deployment clear -a isofhvn/ISOFHCARE-IOS-USER-PRODUCTION Production
+appcenter codepush deployment clear -a isofhvn/ISOFHCARE-ANDROID-USER-TEST Production
+appcenter codepush deployment clear -a isofhvn/ISOFHCARE-IOS-USER-TEST Production
--- release-app.sh
+++ release-app.sh
@@ -1,9 +1,9 @@
-# run: appcenter codepush release-react -a isofhvn/ISOFHCARE-ANDROID-USER-PRODUCTION -d Production -m 
+# run: appcenter codepush release-react -a isofhvn/ISOFHCARE-ANDROID-USER-TEST -d Production -m 
 # nếu bản update là bắt buộc phải cập nhật đến người dùng ngay khi mở app.
 # 
-echo deploy to ISOFHCARE-ANDROID-USER-PRODUCTION
-appcenter codepush release-react -a isofhvn/ISOFHCARE-ANDROID-USER-PRODUCTION -d Production
+echo deploy to ISOFHCARE-ANDROID-USER-TEST
+appcenter codepush release-react -a isofhvn/ISOFHCARE-ANDROID-USER-TEST -d Production
 
-# run: appcenter codepush release-react -a isofhvn/ISOFHCARE-IOS-USER-PRODUCTION -d Production -m 
-echo deploy to ISOFHCARE-IOS-USER-PRODUCTION
-appcenter codepush release-react -a isofhvn/ISOFHCARE-IOS-USER-PRODUCTION -d Production
+# run: appcenter codepush release-react -a isofhvn/ISOFHCARE-IOS-USER-TEST -d Production -m 
+echo deploy to ISOFHCARE-IOS-USER-TEST
+appcenter codepush release-react -a isofhvn/ISOFHCARE-IOS-USER-TEST -d Production
